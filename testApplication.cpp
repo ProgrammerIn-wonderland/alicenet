7a0a6da10868e3c840592b909ff8decd7ca92a44
ProgrammerIn-wonderland <30693865+ProgrammerIn-wonderland@users.noreply.github.com>
2022-06-11 01:32:12 -0400

    Update testApplication.cpp

--- testApplication.cpp
+++ testApplication.cpp
@@ -4,25 +4,36 @@
 
 using namespace std;
 int main() {
-	alicenet request;
-	request.url = "https://httpbin.org/anything";
-	request.type = "GET"; // Optional
-	request.userAgent = "alice/1.0";
-	request.makeRequest();
-	
+     /*
+     * Struct Options are as follows:
+     * 1. User agent (string)
+     * 2. Headers (std::map<std::string, std::string> )
+     * 3. Request Type (string: "GET" or "POST")
+     * 4. authentication (string)(username:password)
+     * 5. post options (string)
+     */
+	alicenetoptions options {
+		"alice/1.0",
+		{},
+		"GET"
+	};
+
+
+	alicenet request("https://httpbin.org/anything");
+
 	cout << "Response Headers:" << endl;
-	for(auto header: request.responseHeaders) {
+	for(auto header: request.response.headers) {
 		cout << header.first << " is " << header.second << endl;
 	}
 	cout << endl << endl;
-	if (request.responseCode == 200) {
-		cout << "Response was successful" << endl; 
+	if (request.response.code == 200) {
+		cout << "Response was successful" << endl;
 	} else {
     	cout << "Response was not successful" << endl << endl;
 	}
 
 	cout << "Reponse Data:" << endl;
 
-	cout << request.responseData;
-	
+	cout << request.response.data;
+
 }
